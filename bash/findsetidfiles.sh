ce9ccb0415c46e56075dd08fcb24d98dd383895d
mayurkumarchaudhari <mayurchaudhari3717@gmail.com>
2022-01-25 02:39:47 -0500

    lab 2

---
+++ bash/findsetidfiles.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# this script generates a report of the files on the system that have the setuid permission bit turned on
+# it is for the lab1 exercise
+# it uses the find command to find files of the right type and with the right permissions, and an error redirect to
+# /dev/null so we do not get errors for inaccessible directories and files
+# the identified files are sorted by their owner
+
+# Task 1 for the exercise is to modify it to also display the 12 largest regular files in the system, sorted by their sizes
+# The listing should
+#    only have the file name, owner, and size of the 12 largest files
+#    show the size in human friendly format
+#    be displayed after the listing of setuid files
+#   should have its own title, similar to how the setuid files listing has a title
+# use the find command to generate the list of files with their sizes, with an error redirect to /dev/null
+# use cut or awk to display only the output desired
+
+echo "Setuid files:"
+echo "============="
+find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 5 | head -12
+echo ""
+
+# for the task, add
+# commands to display a title
+# commands to make a list of the 12 biggest files
+# sort/format whatever to display the list properly
+echo "Mayur Chaudhari"
+echo "200454907"
+echo "************************"
+echo "find files without permission 777"
+find / -type f ! -perm 777 | head -20
+echo ""
+
+
+echo "without 0777 permision top 12 files"
+echo "=======head========"
+find / -type f ! -perm 0777 -print | head -12
+echo ""
+
+echo "without 0777 permision last 12 files"
+echo "=======tail========"
+find . -type f ! -perm 0777 -print | tail -12
+echo ""
